c4883054408f895d79b49bf6031b8e58319e8d4e
LoliweLoliwe <109585257+LoliweLoliwe@users.noreply.github.com>
2024-03-23 15:40:52 +0000

    First commit

---
+++ sketch_morse_code.ino
@@ -0,0 +1,86 @@
+int pin3 = 3;
+int ms500 = 500;
+int seconds = 1500;
+int space = 1500;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin3, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(pin3, HIGH);
+  delay(ms500);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+  digitalWrite(pin3, HIGH);
+  delay(seconds);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+  digitalWrite(pin3, HIGH);
+  delay(ms500);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+  digitalWrite(pin3, HIGH);
+  delay(ms500);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+
+  digitalWrite(pin3, HIGH);
+  delay(seconds);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+  digitalWrite(pin3, HIGH);
+  delay(seconds);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+  digitalWrite(pin3, HIGH);
+  delay(seconds);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+
+  digitalWrite(pin3, HIGH);
+  delay(ms500);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+  digitalWrite(pin3, HIGH);
+  delay(seconds);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+  digitalWrite(pin3, HIGH);
+  delay(ms500);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+  digitalWrite(pin3, HIGH);
+  delay(ms500);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+
+  digitalWrite(pin3, HIGH);
+  delay(ms500);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+  digitalWrite(pin3, HIGH);
+  delay(ms500);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+
+  digitalWrite(pin3, HIGH);
+  delay(ms500);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+  digitalWrite(pin3, HIGH);
+  delay(seconds);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+  digitalWrite(pin3, HIGH);
+  delay(seconds);
+  digitalWrite(pin3, LOW);
+  delay(ms500);
+
+  digitalWrite(pin3, HIGH);
+  delay(ms500);
+  digitalWrite(pin3, LOW);
+  delay(space);
+}
